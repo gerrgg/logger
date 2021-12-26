9635b074f4fba6c94e767e75074cae1d77ee3db5
Greg Bastianelli <greg@gerrg.com>
2021-12-26 14:23:31 -0500

    finish 4.6 & 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require("lodash");
+
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) =>
@@ -15,4 +17,31 @@ const mostLiked = (blogs) => {
   return blogs.length === 0 ? {} : mostLikes;
 };
 
-module.exports = { dummy, totalLikes, mostLiked };
+const mostBlogs = (blogs) => {
+  // create blog object {author: 'greg', blogs: 0;}
+  let authors = _.uniqBy(blogs, "author").map((n) => {
+    return {
+      author: n.author,
+      blogs: blogs.filter(({ author }) => author === n.author).length,
+    };
+  });
+
+  return _.maxBy(authors, "blogs");
+};
+
+const mostLikes = (blogs) => {
+  // create blog object {author: 'greg', blogs: 0;}
+  let authors = _.uniqBy(blogs, "author").map((n) => {
+    return {
+      author: n.author,
+      likes: _.sumBy(
+        blogs.filter(({ author }) => author === n.author),
+        "likes"
+      ),
+    };
+  });
+
+  return authors.length ? _.maxBy(authors, "likes") : {};
+};
+
+module.exports = { dummy, totalLikes, mostLiked, mostBlogs, mostLikes };
